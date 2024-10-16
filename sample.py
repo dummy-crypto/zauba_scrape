90b9cff72eef1524cf42ef662470d4054fdedf88
dummy-crypto <variabledummy3@gmail.com>
2024-10-17 00:12:36 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,27 @@
+mport pandas as pd
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+# Step 1: Authenticate
+gauth = GoogleAuth()
+gauth.LoadClientConfigFile(r'C:\Users\pc\OneDrive\Desktop\client_secret.json')  
+gauth.LocalWebserverAuth()  # Creates a local webserver for authentication
+
+drive = GoogleDrive(gauth)
+
+# Step 2: Create a DataFrame
+df = pd.DataFrame({
+    'Column1': [1, 2, 3],
+    'Column2': ['A', 'B', 'C']
+})
+
+# Step 3: Save the DataFrame as a CSV file locally
+local_csv_path = 'my_data.csv'
+df.to_csv(local_csv_path, index=False)
+
+# Step 4: Upload the CSV file to Google Drive
+file_to_upload = drive.CreateFile({'title': 'my_data.csv'})
+file_to_upload.SetContentFile(local_csv_path)
+file_to_upload.Upload()
+
+print('File uploaded to Google Drive with ID:', file_to_upload.get('id'))
